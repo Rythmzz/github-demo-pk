3454b72abf9e01ad2443014b86c5a9d7af520a09
TuMinh <trantuminh968@gmail.com>
2022-03-15 21:52:33 +0700

    Them tat ca cac file cho tren desktop

---
+++ TestApplet/Applettt.java
@@ -0,0 +1,21 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+
+
+public class Applettt extends Applet{
+  
+    public void Init(){
+        repaint();
+    }
+    public void Start(){
+        
+    }
+    public void Pain(Graphics g){
+        g.drawString("Yep",300, 300);
+    }
+    
+}
+/* 
+<applet code="Applettt.class" width="300" height="300"> 
+</applet> 
+*/
